dd6eea8dde79376aafd4ff8ec49f26faa4374d83
Kent Lui <kent.h.lui@gmail.com>
2016-10-17 05:59:54 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world! this is kent's sample_app"
+  end
+
 end
